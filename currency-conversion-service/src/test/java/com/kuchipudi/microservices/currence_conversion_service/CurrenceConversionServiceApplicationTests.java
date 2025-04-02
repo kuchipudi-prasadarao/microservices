97ccaa3c3dd9a31d52d173d59198669ed1111568
kuchipudiprasadarao <kuchipudiprasadarao@localhost>
2025-04-02 20:12:34 +0530

    initial commit

---
+++ currency-conversion-service/src/test/java/com/kuchipudi/microservices/currence_conversion_service/CurrenceConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuchipudi.microservices.currence_conversion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrenceConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
