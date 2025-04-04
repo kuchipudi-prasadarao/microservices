a8cb690454feae200d3de3b2bb1f0fe5f5352b5d
kuchipudiprasadarao <kuchipudiprasadarao@localhost>
2025-04-04 16:55:59 +0530

    initial commit

---
+++ spring-data-redis-cache/src/main/java/com/kuchipudi/entity/User.java
@@ -0,0 +1,27 @@
+package com.kuchipudi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements java.io.Serializable {
+ private static final long serialVersionUID = 1L;
+ 
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Long id;
+ private String name;
+ private String email;
+
+}
+
